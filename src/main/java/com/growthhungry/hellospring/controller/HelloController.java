7f5a2d5d3b3b39fe94e414491e0bf8485662f976
Eldar Abdivaitov <eldarmeta@outlook.com>
2025-09-30 19:55:09 -0400

    feat(controller): add HelloController with /hello endpoint

--- src/main/java/com/growthhungry/hellospring/controller/HelloController.java
+++ src/main/java/com/growthhungry/hellospring/controller/HelloController.java
@@ -1,4 +1,4 @@
-package com.growthhungry.hellospring;
+package com.growthhungry.hellospring.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
