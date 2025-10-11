b964cec287b525f24046e0e237a883e35ef0a1d4
Eldar Abdivaitov <eldarmeta@outlook.com>
2025-10-11 14:33:25 -0400

    build: project compiles; contextLoads passes an Java 17

--- src/main/java/com/growthhungry/hellospring/repository/UserRepository.java
+++ src/main/java/com/growthhungry/hellospring/repository/UserRepository.java
@@ -2,10 +2,13 @@ package com.growthhungry.hellospring.repository;
 
 import com.growthhungry.hellospring.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
+
     boolean existsByUsername(String username);
+
 }
